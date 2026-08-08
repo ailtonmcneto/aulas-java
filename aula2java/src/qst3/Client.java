daf002b4292d605339811d326c8d25a0ce955687
Ailton Neto <ailtonmcn.dev@gmail.com>
2026-08-08 17:16:57 -0300

    w

--- aula2java/src/qst3/Client.java
+++ aula2java/src/qst3/Client.java
@@ -4,16 +4,15 @@ public class Client {
 
     private String nome, rg, cpf, telefone, endereco, email;
 
-    public Client(String nome, String rg, String cpf, String telefone, String endereco, String email);
+    public Client(String nome, String rg, String cpf, String telefone, String endereco, String email) {
         this.nome = nome;
         this.rg = rg;
         this.cpf = cpf;
         this.telefone = telefone;
         this.endereco = endereco;
         this.email = email;
-
-
+    }
+    
 }
-
 
 
