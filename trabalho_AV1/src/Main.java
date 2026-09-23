1d96d430065ee63532faeb4dbb56faf0b2dcf6c8
Ailton Neto <ailtonmcn.dev@gmail.com>
2026-09-23 20:43:31 -0300

    main for the project

--- trabalho_AV1/src/Main.java
+++ trabalho_AV1/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public static void main(String[] args) {
-    Tarefa tarefa = new Tarefa();
+    Gerenciador tarefa = new Gerenciador();
     Scanner sc = new Scanner(System.in);
     int x;
     do {
@@ -18,14 +18,23 @@ public static void main(String[] args) {
         x = sc.nextInt();
         switch (x) {
             case 0:
+                System.out.println("saindo...");
                 break;
             case 1:
                 tarefa.inserirTarefa();
                 break;
             case 2:
-                tarefa.listarTarefas();
+                tarefa.listarTarefa();
+                break;
+            case 3:
+                tarefa.alterarTarefa();
+                break;
+            case 4:
+                tarefa.removerTarefa();
+                break;
             default:
                 System.out.println("digite uma opção válida");
         }
     } while (x != 0);
+    sc.close();
 }
